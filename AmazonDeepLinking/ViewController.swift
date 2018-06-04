8be306f0c7b703bbb2d6f05586af80153735935b
Saikumar <saikumar@saikumars-macbook-pro.local>
2018-06-04 13:10:47 +0530

    Initial Commit

---
+++ AmazonDeepLinking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmazonDeepLinking
+//
+//  Created by Saikumar on 04/06/18.
+//  Copyright © 2018 hedgehoglab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
